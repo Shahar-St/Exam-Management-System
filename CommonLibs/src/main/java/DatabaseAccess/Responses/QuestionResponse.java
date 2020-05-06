b87831ba3e3434477842bdd3b796dd1196b63b63
Ronnie <ronnieshafran@gmail.com>
2020-05-06 14:22:50 +0300

    temp commit

--- CommonLibs/src/main/java/DatabaseAccess/Responses/QuestionResponse.java
+++ CommonLibs/src/main/java/DatabaseAccess/Responses/QuestionResponse.java
@@ -2,20 +2,27 @@ package DatabaseAccess.Responses;
 
 import DatabaseAccess.Requests.DatabaseRequest;
 
+import java.time.LocalDateTime;
+
 /**
  * Request: asks for a question's full details
  * Response: a string contains the question's full details
  */
 public class QuestionResponse extends DatabaseResponse{
 
-    private final String questionDetails;
+    private final String questionContent;
+    private String[] answers;
+    private int correctAnswer;
+    private String courseName;
+    private String author;
+    private LocalDateTime lastModified;
 
-    public QuestionResponse(boolean status, DatabaseRequest request, String questionDetails) {
+    public QuestionResponse(boolean status, DatabaseRequest request,  ) {
         super(status, request);
-        this.questionDetails = questionDetails;
+        this.questionContent = questionContent;
     }
 
-    public String getQuestionDetails() {
-        return questionDetails;
+    public String getQuestionContent() {
+        return questionContent;
     }
 }
