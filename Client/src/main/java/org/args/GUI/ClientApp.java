df3822113abf8137aad80ae2008efbc969e31a39
r00t <Strong$h!t30>
2020-05-06 19:29:53 +0300

    some of the client functionality

--- Client/src/main/java/org/args/GUI/ClientApp.java
+++ Client/src/main/java/org/args/GUI/ClientApp.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.args.Client.EMSClient;
 
 import java.io.IOException;
 
@@ -15,12 +16,38 @@ public class ClientApp extends Application {
 
     private static Scene scene;
 
+    private static EMSClient client;
+
+    private final String host = "127.0.0.1";
+
+    private final int port = 1337;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void init() throws Exception {
+        try{
+            super.init();
+            client = new EMSClient(this.host,this.port,this);
+        }catch (Exception e){
+            System.out.println("Failed to init app.. exiting");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        try{
+            this.init();
+            scene = new Scene(loadFXML("primary"), 640, 480);
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e){
+            System.out.println("Failed to start the app.. exiting");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -31,6 +58,8 @@ public class ClientApp extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(ClientApp.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
+
+
 
     public static void main(String[] args) {
         launch();
--- Client/src/main/java/org/args/GUI/PrimaryController.java
+++ Client/src/main/java/org/args/GUI/PrimaryController.java
@@ -8,5 +8,6 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         ClientApp.setRoot("secondary");
+
     }
 }
