dd5f62f3ff69041301ef908850af1dc15c1f9b24
Ronnie <ronnieshafran@gmail.com>
2020-05-06 15:23:46 +0300

    temp commit

--- CommonLibs/src/main/java/DatabaseAccess/Responses/QuestionResponse.java
+++ CommonLibs/src/main/java/DatabaseAccess/Responses/QuestionResponse.java
@@ -11,18 +11,44 @@ import java.time.LocalDateTime;
 public class QuestionResponse extends DatabaseResponse{
 
     private final String questionContent;
-    private String[] answers;
-    private int correctAnswer;
-    private String courseName;
-    private String author;
-    private LocalDateTime lastModified;
+    private final String[] answers;
+    private final int correctAnswer;
+    private final String courseName;
+    private final String author;
+    private final LocalDateTime lastModified;
 
-    public QuestionResponse(boolean status, DatabaseRequest request,  ) {
+    public QuestionResponse(boolean status, DatabaseRequest request, String questionContent, String[] answers, int correctAnswer, String courseName, String author, LocalDateTime lastModified  ) {
         super(status, request);
         this.questionContent = questionContent;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+        this.courseName = courseName;
+        this.author = author;
+        this.lastModified = lastModified;
     }
 
     public String getQuestionContent() {
         return questionContent;
     }
+
+    public String[] getAnswers() {
+        return answers;
+    }
+
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public LocalDateTime getLastModified() {
+        return lastModified;
+    }
+
 }
