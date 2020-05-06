8551d1d8a312902656d9246fce6d2fcacdb4cbbc
Ronnie <ronnieshafran@gmail.com>
2020-05-06 16:59:47 +0300

    - LoginScreen.fxml was created and edited (temp version) - LoginScreenController.java was created, waiting to resolve syntax for sending to server.

---
+++ Client/src/main/java/org/args/GUI/LoginScreenController.java
@@ -0,0 +1,32 @@
+/**
+ * Sample Skeleton for 'LoginScreen.fxml' Controller Class
+ */
+
+package org.args.GUI;
+
+import DatabaseAccess.Requests.LoginRequest;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginScreenController {
+
+    @FXML // fx:id="loginButton"
+    private Button loginButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="usernameField"
+    private TextField usernameField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="passwordField"
+    private PasswordField passwordField; // Value injected by FXMLLoader
+
+    @FXML
+    void loginAttempt(ActionEvent event) {
+        LoginRequest loginRequest = new LoginRequest(usernameField.getText(), passwordField.getText());
+
+
+    }
+
+}
