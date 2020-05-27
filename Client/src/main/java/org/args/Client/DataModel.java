bc25d13fe863e9882a0fac72bdc6b64f3aa9063a
Adar <warmundadar@gmail.com>
2020-05-27 18:13:35 +0300

    added required interfaces

--- Client/src/main/java/org/args/Client/DataModel.java
+++ Client/src/main/java/org/args/Client/DataModel.java
@@ -14,9 +14,12 @@ import org.greenrobot.eventbus.Subscribe;
 import java.time.LocalDateTime;
 import java.util.*;
 
-public class DataModel implements IMainScreenData, IQuestionManagementData, IQuestionData {
+public class DataModel implements IMainScreenData, IQuestionManagementData, IQuestionData, IStudentExamExecutionData,
+        IDeanViewStatsData, IStudentViewStatsData, IExamData, ITeacherExamExecutionData,IDeanExamExecutionData,
+        ITeacherViewStatsData,IExamManagementData,IExamReviewData {
 
     private ClientApp app;
+
     public DataModel(ClientApp clientApp) {
         app = clientApp;
         EventBus.getDefault().register(this);
@@ -25,8 +28,8 @@ public class DataModel implements IMainScreenData, IQuestionManagementData, IQue
 
     //teacher main screen data
     @Subscribe
-    public void handleLoginResponse(LoginResponse response){
-        if(response.getStatus()==0){
+    public void handleLoginResponse(LoginResponse response) {
+        if (response.getStatus() == 0) {
             setName(response.getName());
         }
 
@@ -42,8 +45,8 @@ public class DataModel implements IMainScreenData, IQuestionManagementData, IQue
         this.name = name;
     }
 
-    public void login(String userName,String password){
-        ClientApp.sendRequest(new LoginRequest(userName,password));
+    public void login(String userName, String password) {
+        ClientApp.sendRequest(new LoginRequest(userName, password));
     }
 
     @Override
@@ -54,7 +57,7 @@ public class DataModel implements IMainScreenData, IQuestionManagementData, IQue
     //question management screen data - subjects and courses dropdowns
 
     @Subscribe
-    public void handleSubjectsAndCoursesResponse(SubjectsAndCoursesResponse response){
+    public void handleSubjectsAndCoursesResponse(SubjectsAndCoursesResponse response) {
         if (response.getStatus() == 0)
             setSubjectsAndCourses(response.getSubjectsAndCourses());
     }
@@ -84,13 +87,80 @@ public class DataModel implements IMainScreenData, IQuestionManagementData, IQue
         return courseSelected;
     }
 
-    public Set<String> getSubjects()
-    {
+    //TODO: implement IStudentViewStatsData methods
+
+    @Override
+    public List getExams(String courseName) {
+        return null;
+    }
+
+    @Override
+    public void confirmGrade(String examId) {
+
+    }
+
+    @Override
+    public void changeGrade(double newGrade, String reason, String examId) {
+
+    }
+
+    @Override
+    public void editExam(String examId) {
+
+    }
+
+    @Override
+    public void deleteExam(String examId) {
+
+    }
+
+    @Override
+    public void addExam(List questionList) {
+
+    }
+
+    // TODO: decide if the function return void or list, conflict between istudentexam and iexammanagement
+    @Override
+    public void viewExam(String examId) {
+
+    }
+
+    @Override
+    public void addNote(String note) {
+
+    }
+
+    // TODO: implement method for ITeacherViewStatsData
+    @Override
+    public List getTeacherExams() {
+        return null;
+    }
+
+    public Set<String> getSubjects() {
         return subjectsAndCourses.keySet();
     }
 
-    public List<String> getCoursesOfSubject(String subject)
-    {
+    @Override
+    public List getCourses(String subjectName) {
+        return null;
+    }
+
+    @Override
+    public double getAverageGrade(String courseName) {
+        return 0;
+    }
+
+    @Override
+    public void viewExamStatistics(String examId) {
+
+    }
+
+    @Override
+    public void viewReport(String report) {
+
+    }
+
+    public List<String> getCoursesOfSubject(String subject) {
         return subjectsAndCourses.get(subject);
     }
 
@@ -111,19 +181,16 @@ public class DataModel implements IMainScreenData, IQuestionManagementData, IQue
         courseSelected.setValue(true);
     }
 
-    public boolean dataWasAlreadyInitialized()
-    {
+    public boolean dataWasAlreadyInitialized() {
         return currentSubject != null && !subjectsAndCourses.isEmpty();
     }
 
     //question management screen data - questions list data
 
     @Subscribe
-    public void handleAllQuestionsResponse(AllQuestionsResponse response){
-        if (response.getStatus() == 0)
-        {
-            if(observableQuestionsList.size() > 0)
-            {
+    public void handleAllQuestionsResponse(AllQuestionsResponse response) {
+        if (response.getStatus() == 0) {
+            if (observableQuestionsList.size() > 0) {
                 Platform.runLater(new Runnable() {
                     @Override
                     public void run() {
@@ -148,8 +215,7 @@ public class DataModel implements IMainScreenData, IQuestionManagementData, IQue
 
     private final ObservableList<String> observableQuestionsList = FXCollections.observableArrayList();
 
-    private void addToObservableList(String text)
-    {
+    private void addToObservableList(String text) {
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
@@ -186,12 +252,12 @@ public class DataModel implements IMainScreenData, IQuestionManagementData, IQue
     }
 
     //editquestionscreen section
-    private String questionId ;
+    private String questionId;
     private String lastModified;
     private String author;
-    private String content ;
-    private List<String> answers ;
-    private int correctAnswer ;
+    private String content;
+    private List<String> answers;
+    private int correctAnswer;
     private boolean isCreating;
 
     @Override
@@ -266,7 +332,7 @@ public class DataModel implements IMainScreenData, IQuestionManagementData, IQue
         this.choiceItems = choiceItems;
     }
 
-    public void saveQuestion(String questionId,String answer_1,String answer_2,String answer_3,String answer_4,String newContent){
+    public void saveQuestion(String questionId, String answer_1, String answer_2, String answer_3, String answer_4, String newContent) {
         EditQuestionRequest request = new EditQuestionRequest(questionId, newContent,
                 Arrays.asList(answer_1, answer_2, answer_3,
                         answer_4), correctAnswer);
@@ -275,9 +341,9 @@ public class DataModel implements IMainScreenData, IQuestionManagementData, IQue
 
 
     @Subscribe
-    public void handleQuestionResponse(QuestionResponse response){
-        if(response.getStatus()==0){
-            setQuestionId(((QuestionRequest)response.getRequest()).getQuestionID());
+    public void handleQuestionResponse(QuestionResponse response) {
+        if (response.getStatus() == 0) {
+            setQuestionId(((QuestionRequest) response.getRequest()).getQuestionID());
             setLastModified(response.getLastModified().toString());
             setAuthor(response.getAuthor());
             setContent(response.getQuestionContent());
@@ -287,5 +353,47 @@ public class DataModel implements IMainScreenData, IQuestionManagementData, IQue
 
     }
 
+    //TODO: implement IStudentExamExecutionData methods
+    @Override
+    public void storeAnswer(int questionNumber, int answerNumber) {
 
+    }
+
+    @Override
+    public void displayExam() {
+
+    }
+
+    @Override
+    public void submitExam() {
+
+    }
+
+    @Override
+    public void raiseHand() {
+
+    }
+
+    //TODO: implement IExamData Method
+    @Override
+    public void saveExam(String title, List<String> questionList, String examId) {
+
+    }
+
+    //TODO: implement ITeacherExamExecutionData Methods
+    @Override
+    public void timeExtensionRequest(int minutes) {
+
+    }
+
+    @Override
+    public void handleRaisedHand(String studentId) {
+
+    }
+
+    //TODO: implement IDeanExamExecutionData method NOTE: the request should be other type then String!!
+    @Override
+    public void handleTimeExtensionRequest(String request) {
+
+    }
 }
---
+++ Client/src/main/java/org/args/Client/ITeacherExamExecutionData.java
@@ -0,0 +1,6 @@
+package org.args.Client;
+
+public interface ITeacherExamExecutionData {
+    void timeExtensionRequest(int minutes);
+    void handleRaisedHand(String studentId);
+}
