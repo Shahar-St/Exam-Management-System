3ff5b1c4b68d5426eb69c86824dfe73e5de428ec
Ronnie <ronnieshafran@gmail.com>
2020-05-27 17:59:42 +0300

    - added fxml and skeleton controller of exam management and AddExam.

---
+++ Client/src/main/java/org/args/GUI/AddExamScreen.java
@@ -0,0 +1,53 @@
+package org.args.GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class AddExamScreen {
+
+    @FXML
+    private ListView<?> subjectQuestionsListView;
+
+    @FXML
+    private Button detailsButton;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button removeButton;
+
+    @FXML
+    private Button doneButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    void addToExamQuestionList(ActionEvent event) {
+
+    }
+
+    @FXML
+    void allQuestionsAdded(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeFromExamQuestionList(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showQuestionDetails(ActionEvent event) {
+
+    }
+
+}
