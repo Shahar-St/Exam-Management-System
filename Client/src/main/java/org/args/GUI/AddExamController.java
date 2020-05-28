beb8d6e043ab0b1371091b3ad5cd78bfb95846c0
Ronnie <ronnieshafran@gmail.com>
2020-05-28 11:04:55 +0300

    changed "screen" to "controller"

--- Client/src/main/java/org/args/GUI/AddExamController.java
+++ Client/src/main/java/org/args/GUI/AddExamController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
-public class AddExamScreen {
+public class AddExamController {
 
     @FXML
     private ListView<?> subjectQuestionsListView;
