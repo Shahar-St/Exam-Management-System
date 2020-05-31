8165ea57cd17a4d87f7b2a561f672ec591bef848
Ronnie <ronnieshafran@gmail.com>
2020-05-31 14:07:12 +0300

    AddExamScreen fully functional bug in management screen back button

--- Client/src/main/java/org/args/GUI/AddExamController.java
+++ Client/src/main/java/org/args/GUI/AddExamController.java
@@ -1,11 +1,9 @@
 package org.args.GUI;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import org.args.Client.IAddExamData;
@@ -43,17 +41,21 @@ public class AddExamController {
     }
 
     @FXML
-    public void initialize()
-    {
+    public void initialize() {
         setModel(ClientApp.getModel());
-        questionsListTitle.setText("Questions from "+model.getCurrentCourse() +" course");
+        questionsListTitle.setText("Questions from " + model.getCurrentCourse() + " course");
         courseQuestionsListView.setItems(model.getObservableQuestionsList());
         examQuestionsListView.setItems(model.getObservableExamQuestionsList());
+        examQuestionsListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        courseQuestionsListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }
 
     @FXML
     void addToExamQuestionList(ActionEvent event) {
-        model.addToExamQuestionsList(courseQuestionsListView.getSelectionModel().getSelectedItem());
+        for (String selectedItem : courseQuestionsListView.getSelectionModel().getSelectedItems()) {
+            if (!model.getObservableExamQuestionsList().contains(selectedItem))
+                model.addToExamQuestionsList(selectedItem);
+        }
     }
 
     @FXML
@@ -69,13 +71,13 @@ public class AddExamController {
 
     @FXML
     void removeFromExamQuestionList(ActionEvent event) {
-        model.removeFromExamQuestionsList(examQuestionsListView.getSelectionModel().getSelectedItem());
+        for (String selectedItem : examQuestionsListView.getSelectionModel().getSelectedItems())
+            model.removeFromExamQuestionsList(selectedItem);
     }
 
     @FXML
     void showQuestionDetails(ActionEvent event) {
-        if(courseQuestionsListView.getSelectionModel().getSelectedItem() != null)
-        {
+        if (courseQuestionsListView.getSelectionModel().getSelectedItem() != null) {
             int indexOfColon = courseQuestionsListView.getSelectionModel().getSelectedItem().indexOf(':');
             String questionId = courseQuestionsListView.getSelectionModel().getSelectedItem().substring(1, indexOfColon);
             model.saveQuestionDetails(questionId);
@@ -84,11 +86,20 @@ public class AddExamController {
 
     @FXML
     void handleMouseEventCourseQuestionsList(MouseEvent event) {
-        if(!removeButton.isDisabled())
+        if (!removeButton.isDisabled())
             removeButton.setDisable(true);
-        if(event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2 &&
-                event.getTarget() instanceof ListCell)
-            model.addToExamQuestionsList(courseQuestionsListView.getSelectionModel().getSelectedItem());
+        if (addButton.isDisabled())
+            addButton.setDisable(false);
+        examQuestionsListView.getSelectionModel().clearSelection();
+        detailsButton.setDisable(courseQuestionsListView.getSelectionModel().getSelectedItems().size() > 1);
+        if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2 &&
+                event.getTarget() instanceof ListCell && courseQuestionsListView.getSelectionModel().getSelectedItems().size() == 1)
+        {
+            String selectedItem = courseQuestionsListView.getSelectionModel().getSelectedItem();
+            if (!model.getObservableExamQuestionsList().contains(selectedItem))
+                model.addToExamQuestionsList(selectedItem);
+
+        }
 
 
     }
@@ -97,6 +108,13 @@ public class AddExamController {
     void handleMouseEventExamQuestionsList(MouseEvent event) {
         if (removeButton.isDisabled())
             removeButton.setDisable(false);
+        if (!addButton.isDisabled())
+            addButton.setDisable(true);
+        courseQuestionsListView.getSelectionModel().clearSelection();
+        detailsButton.setDisable(examQuestionsListView.getSelectionModel().getSelectedItems().size() > 1);
+        if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2 &&
+                event.getTarget() instanceof ListCell && examQuestionsListView.getSelectionModel().getSelectedItems().size() == 1)
+            model.removeFromExamQuestionsList(examQuestionsListView.getSelectionModel().getSelectedItem());
 
     }
 
