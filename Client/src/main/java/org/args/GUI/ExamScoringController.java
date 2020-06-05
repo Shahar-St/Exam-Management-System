348082e126d3ea6d3e3e6bf35c990c45839af088
Ronnie <ronnieshafran@gmail.com>
2020-06-05 12:43:43 +0300

    ExamScoringScreen is less ugly :)

--- Client/src/main/java/org/args/GUI/ExamScoringController.java
+++ Client/src/main/java/org/args/GUI/ExamScoringController.java
@@ -90,7 +90,7 @@ public class ExamScoringController {
     }
 
     @FXML
-    void backButtonClicked(MouseEvent event) {
+    void backButtonClicked(ActionEvent event) {
         ClientApp.backToLastScene();
 
     }
