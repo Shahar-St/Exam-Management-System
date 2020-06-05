b8d195ee35f753704bf48968ab4671d12570d955
Ronnie <ronnieshafran@gmail.com>
2020-06-05 19:23:36 +0300

    ExamQuestionsController - user not allowed to transition to next screen unless questions were added to the exam.

--- Client/src/main/java/org/args/GUI/ExamQuestionsController.java
+++ Client/src/main/java/org/args/GUI/ExamQuestionsController.java
@@ -59,8 +59,15 @@ public class ExamQuestionsController {
 
     @FXML
     void allQuestionsAdded(ActionEvent event) {
-        ClientApp.setRoot("ExamScoringScreen");
-
+        if (model.getObservableExamQuestionsList().size() >0)
+            ClientApp.setRoot("ExamScoringScreen");
+        else
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setContentText("No questions added!");
+            alert.showAndWait();
+        }
     }
 
     @FXML
