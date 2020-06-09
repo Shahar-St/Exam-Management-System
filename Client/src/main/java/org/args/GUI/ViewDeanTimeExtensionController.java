68e72faf9ad0427803c7b9405b5cf1045f4fc691
Ronnie <ronnieshafran@gmail.com>
2020-06-09 19:02:28 +0300

    temp commit - building the time extension requests system.

---
+++ Client/src/main/java/org/args/GUI/ViewDeanTimeExtensionController.java
@@ -0,0 +1,29 @@
+package org.args.GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class ViewDeanTimeExtensionController {
+
+    @FXML
+    private ListView<String> requestListView;
+
+    @FXML
+    private Button acceptButton;
+
+    @FXML
+    private Button rejectButton;
+
+    @FXML
+    void acceptExtension(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rejectExtension(ActionEvent event) {
+
+    }
+
+}
