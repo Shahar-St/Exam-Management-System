bb02ecd26f656feb60ea6f032c260211d2197933
Ronnie <ronnieshafran@gmail.com>
2020-06-10 12:11:33 +0300

    added examId to the request.

--- CommonLibs/src/main/java/DatabaseAccess/Requests/ExecuteExam/ConfirmTimeExtensionRequest.java
+++ CommonLibs/src/main/java/DatabaseAccess/Requests/ExecuteExam/ConfirmTimeExtensionRequest.java
@@ -7,11 +7,13 @@ public class ConfirmTimeExtensionRequest {
     private final String deanResponse;
     private final int authorizedTimeExtension;
     private final boolean isAccepted;
+    private final String examId;
 
-    public ConfirmTimeExtensionRequest(Boolean isAccepted, String deanResponse, int authorizedTimeExtension) {
+    public ConfirmTimeExtensionRequest(Boolean isAccepted, String deanResponse, int authorizedTimeExtension, String examId) {
         this.deanResponse = deanResponse;
         this.authorizedTimeExtension = authorizedTimeExtension;
         this.isAccepted = isAccepted;
+        this.examId = examId;
     }
 
     public String getDeanResponse() {
@@ -24,5 +26,9 @@ public class ConfirmTimeExtensionRequest {
 
     public boolean isAccepted() {
         return isAccepted;
+    }
+
+    public String getExamId() {
+        return examId;
     }
 }
