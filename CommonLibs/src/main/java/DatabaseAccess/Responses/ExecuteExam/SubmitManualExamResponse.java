1381f259d405f9eed0200460143fec06695b44ee
Shahar Stahi <shaharstahi@gmail.com>
2020-06-16 13:43:54 +0300

    added unmerged classes

---
+++ CommonLibs/src/main/java/DatabaseAccess/Responses/ExecuteExam/SubmitManualExamResponse.java
@@ -0,0 +1,18 @@
+package DatabaseAccess.Responses.ExecuteExam;
+
+import DatabaseAccess.Requests.DatabaseRequest;
+import DatabaseAccess.Responses.DatabaseResponse;
+
+/**
+ * verifies the exam's submission was successful.
+ * status dictionary:
+ * 0 - success
+ * 1 - unauthorized access - user isn't logged in
+ * 2 - exam wasn't found
+ */
+
+public class SubmitManualExamResponse extends DatabaseResponse {
+    public SubmitManualExamResponse(int status, DatabaseRequest request) {
+        super(status, request);
+    }
+}
